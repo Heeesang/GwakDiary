bc1bdd574f9d017dddc41405f9f9c2b9d839685d
Heesang <ghs050611@naver.com>
2022-10-03 23:28:35 +0900

    :lipstick: addImageButton

--- Project/Source/Presentation/Scene/ReadDiary/VC/ReadDiaryViewController.swift
+++ Project/Source/Presentation/Scene/ReadDiary/VC/ReadDiaryViewController.swift
@@ -2,4 +2,19 @@ import UIKit
 
 class ReadDiaryViewController: baseVC<ReadDiaryViewModel> {
     
+    private let addImageButton = UIButton().then {
+        $0.layer.cornerRadius = 13
+        $0.layer.cornerRadius = 10
+        $0.layer.shadowRadius = 15
+        $0.layer.shadowOffset =
+        CGSize(width: 0, height: 0)
+        $0.layer.shadowOpacity = 1
+        $0.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.08).cgColor
+    }
+    
+//    private let writeDiaryContainerView = UIView().then {
+//        
+//    }
+        
+    
 }
